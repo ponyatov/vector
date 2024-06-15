be5f3e517fd07169586fbd0ddca41addeb50d3dc
Dmitry Ponyatov <dponyatov@gmail.com>
2024-06-15 10:17:49 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -2,11 +2,17 @@
 #![allow(unused_variables)]
 
 use std::net::TcpListener;
+use chrono::Local;
 
 const IP:&str = "127.0.0.1";
 const PORT: i16 = 12345;
 
 fn main() {
     println!("server @ http://{IP}:{PORT}");
-    let listener = TcpListener::bind(format!("{IP}:{PORT}"));
+    let listener = TcpListener::bind(format!("{IP}:{PORT}")).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let now = chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
+        println!("{now} stream");
+    }
 }
