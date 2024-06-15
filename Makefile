af42d9cfb21d1af73fd16b6b60e59d9bc0e72855
Dmitry Ponyatov <dponyatov@gmail.com>
2024-06-15 09:50:09 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -11,3 +11,13 @@ all:
 format: tmp/format_rs
 tmp/format_rs: $(R)
 	cargo run fmt $? && touch $@
+
+# install
+.PHONY: install update gz ref
+install: gz ref
+	$(MAKE) update
+update:
+	sudo apt update
+	sudo apt install -uy `cat apt.txt`
+gz:
+ref:
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::net::TcpListener;
+
+const IP:&str = "127.0.0.1";
+const PORT: i16 = 12345;
+
 fn main() {
-    println!("Hello, world!");
+    println!("server @ http://{IP}:{PORT}");
+    let listener = TcpListener::bind(format!("{IP}:{PORT}"));
 }
